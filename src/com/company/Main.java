0d16f5b9478d4043b48a74a4cbfafe05623e2240
kubatAshymjanov <94351472+kubatAshymjanov@users.noreply.github.com>
2022-02-18 12:57:37 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Triangle triangle = new Triangle();
+        triangle.a=15;
+        triangle.b=10;
+        triangle.c=10;
+        triangle.area();
+    }
+}
